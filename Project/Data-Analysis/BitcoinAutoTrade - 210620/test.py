1a859dbd7bb04bc885552954a765b970649b4b99
Hayeon Kim <85288036+HayeonKimm@users.noreply.github.com>
2021-12-20 23:48:46 +0900

    Delete Project/Data-Analysis/BitcoinAutoTrade - 210620 directory

--- Project/Data-Analysis/BitcoinAutoTrade - 210620/test.py
+++
@@ -1,9 +0,0 @@
-import pyupbit
-
-access = "ppWOs872nz4KdThVEZzRxNWL8VJVopILUrlZms1b"          # 본인 값으로 변경
-secret = "xXktltWqbDDVB47osoj0pSTTBjLjwSdxLryARS6k"          # 본인 값으로 변경
-upbit = pyupbit.Upbit(access, secret)
-
-
-print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
